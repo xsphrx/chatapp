6b190445b76d002229781e9e34e3ac35ed3bb5bc
xsphrx <przemek.maranowski@gmail.com>
2019-07-09 15:53:58 +0200

    Updates

---
+++ precache-manifest.6fd2c00450f872049e0a756639f62526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37a98e2d0713181be1f4ef23bfcebbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca936e66d90acf816c8",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "5f62c364f3ff846d75d3",
+    "url": "/static/css/main.6b34aae2.chunk.css"
+  },
+  {
+    "revision": "5ca936e66d90acf816c8",
+    "url": "/static/js/2.1ede5e3a.chunk.js"
+  },
+  {
+    "revision": "5f62c364f3ff846d75d3",
+    "url": "/static/js/main.77e05958.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
